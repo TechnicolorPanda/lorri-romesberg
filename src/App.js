35c165c104974475bbe80ee19c88733fa52bdb58
LorriAnn <LorriAnn7@att.net>
2021-08-25 22:42:29 -0400

    Creates home page.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React from 'react';
-import './styles/App.css';
-// import Home from './components/Home';
-// import Nav from './components/Nav';
-// import About from './components/About';
-// import Skills from './components/Skills';
-// import Projects from './components/Projects';
-// import Contact from './components/Contact';
-import { BrowserRouter as Router, Switch, 
-  // Route 
-} from 'react-router-dom';
+import './App.css';
 
 function App() {
 
   return (
-    <Router>
       <div className = 'App'>
-        <div className = 'navbar'>
-          {/* <Nav/> */}
-        </div>
-        <div className = 'content'>
-          <Switch>
-            {/* <Route path = '/' exact component = {Home} />
-            <Route path = '/about' component = {About}/>
-            <Route path = '/skills' exact component = {Skills}/>
-            <Route path = '/projects' component = {Projects}/>
-            <Route path = '/contact' component = {Contact}/> */}
-          </Switch>
-        </div>
+        <h1>
+          <p>Lorri </p>
+          <p>Romesberg</p>
+        </h1>
+        <h3>full stack web developer</h3>
       </div>
-    </Router>
   );
 }
 
