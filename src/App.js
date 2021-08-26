725436ad64b7e7c6f14dbdc4e8c2f8766fc1624a
LorriAnn <LorriAnn7@att.net>
2021-08-25 21:57:15 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/App.css';
+// import Home from './components/Home';
+// import Nav from './components/Nav';
+// import About from './components/About';
+// import Skills from './components/Skills';
+// import Projects from './components/Projects';
+// import Contact from './components/Contact';
+import { BrowserRouter as Router, Switch, 
+  // Route 
+} from 'react-router-dom';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className = 'App'>
+        <div className = 'navbar'>
+          {/* <Nav/> */}
+        </div>
+        <div className = 'content'>
+          <Switch>
+            {/* <Route path = '/' exact component = {Home} />
+            <Route path = '/about' component = {About}/>
+            <Route path = '/skills' exact component = {Skills}/>
+            <Route path = '/projects' component = {Projects}/>
+            <Route path = '/contact' component = {Contact}/> */}
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
