2722583282038dfcb82036276e757523c2193f63
LorriAnn <LorriAnn7@att.net>
2021-08-26 11:39:35 -0400

    Adds name animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
 
+  useEffect(() => {
+    document.getElementById('first-name').style.marginLeft = '10%';
+    document.getElementById('last-name').style.marginLeft = '10%';
+  },[]);
+
   return (
       <div className = 'App'>
         <h1>
-          <p>Lorri </p>
-          <p>Romesberg</p>
+          <div id = 'first-name'>Lorri </div>
+          <div id = 'last-name'>Romesberg</div>
         </h1>
         <h3>full stack web developer</h3>
       </div>
